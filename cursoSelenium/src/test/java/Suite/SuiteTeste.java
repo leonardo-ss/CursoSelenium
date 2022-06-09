c5830daf1088fc75f5e3976896d243ba239b63f6
leoss <leo.silva.94@hotmail.com>
2022-06-09 12:16:37 -0300

    finalizando

--- cursoSelenium/src/test/java/Suite/SuiteTeste.java
+++ cursoSelenium/src/test/java/Suite/SuiteTeste.java
@@ -1,19 +1,23 @@
 package Suite;
+import org.junit.AfterClass;
 import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 import org.junit.runners.Suite.SuiteClasses;
 
 import Teste.TesteCadastro;
-import Teste.TesteCampoTreinamento;
 import Teste.TesteRegrasCadastro;
+import core.DriverFactory;
 
 @RunWith(Suite.class)
 @SuiteClasses({
     TesteCadastro.class,
-    TesteRegrasCadastro.class,
-    TesteCampoTreinamento.class
+    TesteRegrasCadastro.class
 })
 public class SuiteTeste {
     
+    @AfterClass
+    public static void finalizaTudo(){
+        DriverFactory.killDriver();
+    }
 
 }
